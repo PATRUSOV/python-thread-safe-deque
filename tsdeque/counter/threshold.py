d93650c1629cc2a5e157e1e1198782ee94ad1a39
kotmarkot <andrey.patrusov@gmail.com>
2025-08-01 13:04:14 +0000

    fix: Исправлен некоррекный импорт.

--- tsdeque/counter/threshold.py
+++ tsdeque/counter/threshold.py
@@ -1,11 +1,8 @@
-from typing import Union, TYPE_CHECKING
+from typing import Union
 from dataclasses import dataclass
 
-from tsdeque.counter.nulldevent import _NULL_DEVENT
-
-if TYPE_CHECKING:
-    from tsdeque.devent import Devent
-    from tsdeque.counter.nulldevent import _NullDevent
+from tsdeque.counter.nulldevent import _NULL_DEVENT, _NullDevent
+from tsdeque.devent import Devent
 
 
 @dataclass
