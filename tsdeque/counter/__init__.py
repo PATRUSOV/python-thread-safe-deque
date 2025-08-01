233c52efd1d6dd57182f9ddaeb1eb3c0b1444b74
kotmarkot <andrey.patrusov@gmail.com>
2025-08-01 12:44:44 +0000

    fix: В __init__ добавлен Threshold.

--- tsdeque/counter/__init__.py
+++ tsdeque/counter/__init__.py
@@ -1,4 +1,5 @@
 from tsdeque.counter.core import Counter
+from tsdeque.counter.threshold import Threshold
 from tsdeque.counter.exceptions import LowThresholdError, HighThresholdError
 
-__all__ = ["Counter", "LowThresholdError", "HighThresholdError"]
+__all__ = ["Counter", "Threshold", "LowThresholdError", "HighThresholdError"]
